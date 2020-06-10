735e2580a50db70ad02348848064c4826e7270d2
Ben <1481777068@qq.com>
2020-06-10 16:14:04 +0800

    测试

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+
+    int num = 0;
+    int& a = num;
+    a = 20;
+    cout << num << endl;
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
